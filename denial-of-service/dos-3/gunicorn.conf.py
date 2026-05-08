0baea4411fe78eb741c7252f596bc46fff17e7ec
Fluffy <52132640+fluffy-awoo@users.noreply.github.com>
2026-05-07 20:25:08 -0700

    Update DOS

--- denial-of-service/dos-3/gunicorn.conf.py
+++ denial-of-service/dos-3/gunicorn.conf.py
@@ -1,5 +1,7 @@
 wsgi_app = "wsgi:application"
-bind = "0.0.0.0:80"
+bind = "challenge.internal:443"
+certfile = "/etc/ssl/certs/internal.cert.pem"
+keyfile = "/etc/ssl/private/internal.key.pem"
 loglevel = "info"
 accesslog = "/var/log/gunicorn/access.log"
 errorlog = "/var/log/gunicorn/error.log"
